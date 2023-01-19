73b62caa17eb8087ea870b3c3fbd5dea8f787b57
unknown <1175811@hae.hmgdaas.global>
2023-01-19 10:38:28 +0900

    sample code upload

---
+++ spring/src/main/java/demo/helloworld/controller/HelloworldController.java
@@ -0,0 +1,40 @@
+package demo.helloworld.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloworldController {
+    @GetMapping("hello-thymeleaf")
+    public String HelloThymeleaf(Model model) {
+        model.addAttribute("data", "thymeleaf");
+        return "hello-thymeleaf";
+    }
+
+    @GetMapping("hello-get")
+    public String HelloGet(@RequestParam(value = "data", required = false) String data, Model model) {
+        model.addAttribute("data", data);
+        return "hello-thymeleaf";
+    }
+
+    @GetMapping("hello-body")
+    @ResponseBody
+    public String HelloBody(@RequestParam(value = "data", required = false) String data, Model model) {
+        return "hello " + data + "!";
+    }
+
+    @GetMapping("hello-json")
+    @ResponseBody
+    public Hello HelloJson(@RequestParam(value = "data", required = false) String data, Model model) {
+        Hello h = new Hello();
+        h.data = data;
+        return h;
+    }
+
+    static class Hello {
+        public String data;
+    }
+}
