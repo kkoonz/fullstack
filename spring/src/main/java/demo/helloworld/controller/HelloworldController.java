5b03be8c63860d3c1f89dfc1261c8dc4ae50933c
kkoonz <kkoon79@gmail.com>
2023-02-02 19:13:11 +0900

    URL 파라메터 샘플 추가

--- spring/src/main/java/demo/helloworld/controller/HelloworldController.java
+++ spring/src/main/java/demo/helloworld/controller/HelloworldController.java
@@ -6,9 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -61,5 +59,11 @@ public class HelloworldController {
     public List<HelloMybatisEntity> list() throws Exception {
         List<HelloMybatisEntity> list = mDAO.list();
         return list;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path="hello-url/{data}")
+    @ResponseBody
+    public String helloURL(@PathVariable("data") String data) {
+        return "Hello " + data;
     }
 }
